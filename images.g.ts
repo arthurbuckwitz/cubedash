bd094fe6c39f7c7ff7c4d860492ccd29a8563247
arthurbuckwitz <159729858+arthurbuckwitz@users.noreply.github.com>
2024-02-12 17:14:51 -0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "design1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f f . . . . 
+. . . f f 1 2 8 8 2 1 f f . . . 
+. . . f 1 f f f f f f 1 f . . . 
+. . . f 2 f 2 2 2 2 f 2 f . . . 
+. . . f 8 f 2 8 8 2 f 8 f . . . 
+. . . 1 8 1 2 8 8 2 1 8 1 . . . 
+. . . 1 2 1 2 2 2 2 1 2 1 . . . 
+. . . 1 f 1 1 1 1 1 1 f 1 . . . 
+. . . 1 1 f 2 8 8 2 f 1 1 . . . 
+. . . . 1 1 1 1 1 1 1 1 . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
